ba67a1d464a38457d8aee022376c4a5c1ee917d7
Nick Lammers <nlammers@berkeley.edu>
2023-05-22 14:33:49 -0700

    Updates to VAE scripts and data build

--- apply_vae.py
+++ apply_vae.py
@@ -1,4 +1,5 @@
 # save the model
+
 import torch
 import matplotlib.pyplot as plt
 import numpy as np
@@ -16,9 +17,13 @@ Determine if any GPUs are available
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # set path to model and data
-modeldir = "/Users/nick/Dropbox (Cole Trapnell's Lab)/Nick/morphSeq/data/vae_test/"
-datadir = "/Users/nick/Dropbox (Cole Trapnell's Lab)/Nick/morphSeq/data/vae_test/max_images_res014/"
-outdir = modeldir + "max_images_res014_pd/"
+model_name = '20230519_vae_max_z050'
+datadir = "E:/Nick/Dropbox (Cole Trapnell's Lab)/Nick/morphSeq/data/vae_test/20230519_max_images_res014/"
+modeldir = "E:/Nick/Dropbox (Cole Trapnell's Lab)/Nick/morphSeq/data/vae_test/" + model_name + "/"
+zd = int(model_name[-3:])
+# datadir = "E:/Nick/Dropbox (Cole Trapnell's Lab)/Nick/morphSeq/data/vae_test/max_images_res014/"
+# modeldir = "E:/Nick/Dropbox (Cole Trapnell's Lab)/Nick/morphSeq/data/vae_test/" + model_name + "/"
+outdir = modeldir + "depth_images_res014_pd/" + model_name + "/"
 
 if not os.path.isdir(outdir):
     os.makedirs(outdir)
@@ -32,15 +37,15 @@ data_loader = torch.utils.data.DataLoader(im_data, batch_size=batch_size)
 
 # Model class must be defined somewhere
 net = VAE().to(device)
-net.load_state_dict(torch.load(modeldir + '20230516_vae01'))
+net.load_state_dict(torch.load(modeldir + model_name))
 net.eval()
 
-mu_array = np.empty((len(data_loader), 50))
+mu_array = np.empty((len(data_loader), zd))
 
 with torch.no_grad():
     for idx, data in enumerate(list(data_loader)):
         im_path = im_data.samples[idx][0]
-        im_name = im_path.replace(datadir + 'class0/', '')
+        im_name = im_path.replace(datadir + 'class0\\', '')
         im_name = im_name.replace('.tif', '')
 
         imgs, _ = data
@@ -54,7 +59,8 @@ with torch.no_grad():
         plt.yticks([])
         plt.title('Original')
 
-        out, mu_array[idx, :], logVAR = net(imgs)
+        out, mu, logVAR = net(imgs)
+        mu_array[idx, :] = mu.cpu()
         outimg = np.transpose(out[0].cpu().numpy(), [1, 2, 0])
 
         plt.subplot(122)
