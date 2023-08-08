f576a35f81720ca2d69fbb6b7ec6ead1f4ec2ee7
nlammers371 <nlammers@berkeley.edu>
2023-08-08 12:14:39 -0700

    Implemented ability to dynamically specify NN depth, as well as input image size

--- functions/pythae_utils.py
+++ functions/pythae_utils.py
@@ -13,16 +13,16 @@ data_transform = transforms.Compose([
     transforms.ToTensor() # the data must be tensors
 ])
 
-data_transform_rs = transforms.Compose([
-    transforms.Grayscale(num_output_channels=1),
-    transforms.Resize((128, 128), antialias=False),
-    transforms.ToTensor(),
-    # the data must be tensors
-])
+def make_dynamic_rs_transform(im_dims):
+    data_transform = transforms.Compose([
+        transforms.Grayscale(num_output_channels=1),
+        transforms.Resize((im_dims[0], im_dims[1]), antialias=False),
+        transforms.ToTensor(),
+    ])
+    return data_transform
 
 #########3
 # Define a custom dataset class
-
 class MyCustomDataset(datasets.ImageFolder):
 
     def __init__(self, root, transform=None, target_transform=None):
@@ -35,14 +35,13 @@ class MyCustomDataset(datasets.ImageFolder):
             data=X
         )
 
-
 ##########
 # Define custom convolutional encoder that allows for variable input size
 def conv_output_shape(h_w, kernel_size=1, stride=1, pad=0, dilation=1):
     if type(kernel_size) is not tuple:
         kernel_size = (kernel_size, kernel_size)
-    h = floor( ((h_w[0] + (2 * pad) - ( dilation * (kernel_size[0] - 1) ) - 1 )/ stride) + 1)
-    w = floor( ((h_w[1] + (2 * pad) - ( dilation * (kernel_size[1] - 1) ) - 1 )/ stride) + 1)
+    h = floor(((h_w[0] + (2 * pad) - (dilation * (kernel_size[0] - 1)) - 1) / stride) + 1)
+    w = floor(((h_w[1] + (2 * pad) - (dilation * (kernel_size[1] - 1)) - 1) / stride) + 1)
     return h, w
 
 def deconv_output_shape(h_w, kernel_size=1, stride=1, pad=0, dilation=1, output_padding=1):
@@ -53,15 +52,22 @@ def deconv_output_shape(h_w, kernel_size=1, stride=1, pad=0, dilation=1, output_
     return h_out, w_out
 
 class Encoder_Conv_VAE_FLEX(BaseEncoder):
-    def __init__(self, init_config, n_out_channels=16, stride=2, kernel_size=4):
+    def __init__(self, init_config, n_conv_layers=4, n_out_channels=16):
         BaseEncoder.__init__(self)
 
-        self.input_dim = init_config.input_dim # (1, 28, 28)
+        stride = 2  # I'm keeping this fixed at 2 for now
+        kernel_size = 4 # Keep fixed at
+
+        self.n_out_channels = n_out_channels
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.n_conv_layers = n_conv_layers
+
+        self.input_dim = init_config.input_dim
         self.latent_dim = init_config.latent_dim
         self.n_channels = self.input_dim[0]
 
         # get predicted output size of base image
-        n_conv_layers = 4
         [ht, wt] = self.input_dim[1:]
         for n in range(n_conv_layers):
             [ht, wt] = conv_output_shape([ht, wt], kernel_size=kernel_size, stride=stride, pad=1)
@@ -69,21 +75,19 @@ class Encoder_Conv_VAE_FLEX(BaseEncoder):
         # use this to calculate feature size
         featureDim = ht*wt*n_out_channels*2**(n_conv_layers-1)
 
-        self.conv_layers = nn.Sequential(
-            nn.Conv2d(self.n_channels, out_channels=n_out_channels, kernel_size=kernel_size, stride=stride, padding=1),
-            nn.BatchNorm2d(n_out_channels),
-            nn.ReLU(),
-            nn.Conv2d(n_out_channels, 2*n_out_channels, kernel_size, stride, padding=1),
-            nn.BatchNorm2d(2*n_out_channels),
-            nn.ReLU(),
-            nn.Conv2d(2*n_out_channels, 4*n_out_channels, kernel_size, stride, padding=1),
-            nn.BatchNorm2d(4*n_out_channels),
-            nn.ReLU(),
-            nn.Conv2d(4*n_out_channels, 8*n_out_channels, kernel_size, stride, padding=1),
-            nn.BatchNorm2d(8*n_out_channels),
-            nn.ReLU(),
-        )
+        self.conv_layers = nn.Sequential()
+        for n in range(n_conv_layers):
+            if n == 0:
+                n_in = self.n_channels
+            else:
+                n_in = n_out_channels*2**(n-1)
+            n_out = n_out_channels*2**n
 
+            self.conv_layers.append(nn.Conv2d(n_in, out_channels=n_out, kernel_size=kernel_size, stride=stride, padding=1))
+            self.conv_layers.append(nn.BatchNorm2d(n_out))
+            self.conv_layers.append(nn.ReLU())
+
+        # add latent layers
         self.embedding = nn.Linear(featureDim, self.latent_dim)
         self.log_var = nn.Linear(featureDim, self.latent_dim)
 
@@ -97,36 +101,48 @@ class Encoder_Conv_VAE_FLEX(BaseEncoder):
 
 
 class Decoder_Conv_AE_FLEX(BaseDecoder):
-    def __init__(self, base_config, n_out_channels=16, stride=2, kernel_size=4):
+    def __init__(self, encoder_config):
         BaseDecoder.__init__(self)
 
-        self.input_dim = base_config.input_dim  # (1, 28, 28)
-        self.latent_dim = base_config.latent_dim
+        n_out_channels = encoder_config.n_out_channels
+        kernel_size = encoder_config.kernel_size
+        stride = encoder_config.stride
+        n_conv_layers = encoder_config.n_conv_layers
+
+        self.input_dim = encoder_config.input_dim  # (1, 28, 28)
+        self.latent_dim = encoder_config.latent_dim
         self.n_channels = self.input_dim[0]
 
         # get predicted output size of base image
-        n_conv_layers = 4
         [ht, wt] = self.input_dim[1:]
         for n in range(n_conv_layers):
             [ht, wt] = conv_output_shape([ht, wt], kernel_size=kernel_size, stride=stride, pad=1)
         self.h_base = ht*2  # NL: factor of 2 is because we have one fewer conv layer in decoder
         self.w_base = wt*2
+
         # use this to calculate feature size
         featureDim = 4 * ht * wt * n_out_channels * 2 ** (n_conv_layers - 1)
         self.featureDim = featureDim
 
         # self.fc = nn.Linear(self.latent_dim, featureDim * 4 * 4)  # not sure where this factor of 16 comes from
         self.fc = nn.Linear(self.latent_dim, featureDim)
-        self.deconv_layers = nn.Sequential(
-            nn.ConvTranspose2d(8*n_out_channels, 4*n_out_channels, 4, 2, padding=1),
-            nn.BatchNorm2d(4*n_out_channels),
-            nn.ReLU(),
-            nn.ConvTranspose2d(4*n_out_channels, 2*n_out_channels, 4, 2, padding=1, output_padding=0),
-            nn.BatchNorm2d(2*n_out_channels),
-            nn.ReLU(),
-            nn.ConvTranspose2d(2*n_out_channels, self.n_channels, 4, 2, padding=1, output_padding=0),
-            nn.Sigmoid(),
-        )
+
+        self.deconv_layers = nn.Sequential()
+        for n in range(1, n_conv_layers):
+            p_ind = n_conv_layers - n
+            if n == n_conv_layers-1:
+                n_out = self.n_channels
+            else:
+                n_out = n_out_channels*2**(p_ind-1)
+            n_in = n_out_channels*2**p_ind
+
+            self.deconv_layers.append(nn.ConvTranspose2d(n_in, n_out, kernel_size, stride, padding=1))
+
+            if n == n_conv_layers-1:
+                self.deconv_layers.append(nn.Sigmoid())
+            else:
+                self.deconv_layers.append(nn.BatchNorm2d(n_out))
+                self.deconv_layers.append(nn.ReLU())
 
     def forward(self, z: torch.Tensor):
         h1 = self.fc(z).reshape(z.shape[0], int(self.featureDim / self.w_base / self.h_base), self.h_base, self.w_base)
