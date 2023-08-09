20e8698fd03b989de32925a95127adec6ddf1076
Nicholas Lammers <nlammers@berkeley.edu>
2023-08-09 14:19:27 -0700

    Draft of napari vis script

---
+++ visualization/napari_visualization.py
@@ -0,0 +1,27 @@
+from aicsimageio import AICSImage
+import numpy as np
+import napari
+# read the image data
+from ome_zarr.io import parse_url
+
+
+full_filename = "/Users/nick/Dropbox (Cole Trapnell's Lab)/Nick/yx1_temp/10x_emilin3a-mScarlet_notochord_zstep05_bright_bf_0005.nd2"
+
+imObject = AICSImage(full_filename)
+# imObject.set_scene("XYPos:17")
+# imData = np.squeeze(imObject.get_image_data("CZYX", T=13))
+imData = np.squeeze(imObject.data)
+
+# Extract pixel sizes and bit_depth
+res_raw = imObject.physical_pixel_sizes
+res_array = np.asarray(res_raw)
+
+#
+# # with open("/Users/nick/RNA300_GFP_10x_wholeEmbryo.npy", 'wb') as f:
+# # np.save("/Users/nick/RNA300_GFP_10x_wholeEmbryo.npy", imData)
+#
+viewer = napari.view_image(imData, colormap="green", scale=res_array)
+
+# # labels_layer = viewer.add_labels(lbData, name='segmentation', scale=res_array)
+if __name__ == '__main__':
+    napari.run()
