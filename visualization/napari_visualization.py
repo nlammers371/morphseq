47c9eb5cd26b413799ea85c4af7fbe9e8b46741f
Nicholas Lammers <nlammers@berkeley.edu>
2023-08-28 14:54:09 -0700

    Old changes

--- visualization/napari_visualization.py
+++ visualization/napari_visualization.py
@@ -6,11 +6,12 @@ from ome_zarr.io import parse_url
 
 
 # full_filename = "/Users/nick/Dropbox (Cole Trapnell's Lab)/Nick/yx1_temp/10x_emilin3a-mScarlet_notochord_zstep05_bright_bf_0005.nd2"
-full_filename = "/Users/nick/Dropbox (Cole Trapnell's Lab)/Nick/yx1_temp/20230817/20x_E03_48hpf_tdTom_fin.nd2"
-
+# full_filename = "/Users/nick/Dropbox (Cole Trapnell's Lab)/Nick/yx1_temp/20230817/20x_E03_48hpf_tdTom_fin.nd2"
+full_filename = "/Volumes/LaCie/40x_fin_tdTom_ZF_pec_fin.nd2"
+# full_filename = "/Volumes/LaCie/tdTom_ZF_pec_fin.nd2"
 imObject = AICSImage(full_filename)
-# imObject.set_scene("XYPos:17")
-# imData = np.squeeze(imObject.get_image_data("CZYX", T=13))
+# imObject.set_scene("XYPos:4")
+# imData = np.squeeze(imObject.get_image_data("TZYX", C=0))
 imData = np.squeeze(imObject.data)
 
 # Extract pixel sizes and bit_depth
