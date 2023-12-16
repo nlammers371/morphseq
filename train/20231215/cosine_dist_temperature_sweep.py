0b7307cc5643bff3fb022619cc54e55f3a419aff
nlammers371 <nlammers@grid-head1.grid.gs.washington.edu>
2023-12-15 17:24:24 -0800

    Added script to train cosine-based metric and seqVAE models

--- train/20231215/cosine_dist_temperature_sweep.py
+++ train/20231215/cosine_dist_temperature_sweep.py
@@ -15,21 +15,22 @@ if __name__ == "__main__":
     root = "/net/trapnell/vol1/home/nlammers/projects/data/morphseq/"
     train_folder = "20231106_ds"
     train_dir = os.path.join(root, "training_data", train_folder)
-    # model_type = "MetricVAE"
+    model_type_vec = ["SeqVAE", "MetricVAE"]
 
     #####################
     # Optional arguments
-    train_suffix = "temperature_sweep"
-    temperature = [0.0001, 0.001, 0.01, 0.1, 1]
+    train_suffix = "cosine_temperature_sweep"
+    temperature_vec = [0.0001, 0.001, 0.01, 0.1, 1]
     batch_size = 64
     n_epochs = 250
     beta = 1 #[0.01, 0.1, 1]
     latent_dim = 100
     n_conv_layers = 5
-    distance_metric = "euclidean"
+    distance_metric = "cosine"
     input_dim = (1, 288, 128)
 
-
-    output_dir = train_vae(root, train_folder, train_suffix=train_suffix, model_type=model_type,
-                           latent_dim=latent_dim, batch_size=batch_size, beta=beta,  n_load_workers=4,
-                           n_epochs=n_epochs, temperature=temperature, learning_rate=1e-4, n_conv_layers=n_conv_layers)
+    for model_type in model_type_vec:
+        for temperature in temperature_vec:
+            output_dir = train_vae(root, train_folder, train_suffix=train_suffix, model_type=model_type,
+                                latent_dim=latent_dim, batch_size=batch_size, beta=beta,  n_load_workers=4, distance_metric="cosine",
+                                n_epochs=n_epochs, temperature=temperature, learning_rate=1e-4, n_conv_layers=n_conv_layers)
