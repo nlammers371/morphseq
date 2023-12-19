8424e548662feb6c96a6bd1a0d0054536d2813db
nlammers371 <nlammers@t001.grid.gs.washington.edu>
2023-12-18 18:18:25 -0800

    Commented out time inference in assessment script

--- train/20231218/train_vanilla_vae.py
+++ train/20231218/train_vanilla_vae.py
@@ -15,12 +15,11 @@ if __name__ == "__main__":
     root = "/net/trapnell/vol1/home/nlammers/projects/data/morphseq/"
     train_folder = "20231106_ds"
     train_dir = os.path.join(root, "training_data", train_folder)
-    model_type_vec = ["SeqVAE", "MetricVAE"]
+    model_type = "VAE"
 
     #####################
     # Optional arguments
-    train_suffix = "cosine_temperature_sweep"
-    temperature_vec = [0.0001, 0.001, 0.01, 0.1, 1]
+    train_suffix = "vanilla"
     batch_size = 64
     n_epochs = 250
     beta = 1 #[0.01, 0.1, 1]
@@ -29,8 +28,8 @@ if __name__ == "__main__":
     distance_metric = "cosine"
     input_dim = (1, 288, 128)
 
-    for model_type in model_type_vec:
-        for temperature in temperature_vec:
-            output_dir = train_vae(root, train_folder, train_suffix=train_suffix, model_type=model_type,
-                                latent_dim=latent_dim, batch_size=batch_size, beta=beta,  n_load_workers=4,
-                                n_epochs=n_epochs, temperature=temperature, learning_rate=1e-4, n_conv_layers=n_conv_layers, distance_metric="cosine")
+
+
+    output_dir = train_vae(root, train_folder, train_suffix=train_suffix, model_type=model_type,
+                        latent_dim=latent_dim, batch_size=batch_size, beta=beta,  n_load_workers=4,
+                        n_epochs=n_epochs, learning_rate=1e-4, n_conv_layers=n_conv_layers)
