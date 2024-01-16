1c65ddb2973c0df4138fc95947a7cec6228730dd
nlammers371 <nlammers@t006.grid.gs.washington.edu>
2024-01-16 14:23:55 -0800

    Good progress toward script for extracting YX1 images and metadata. Need to finishe resizing, FF calc, and metadata export

--- src/build/01B_compile_yx1_images_par.py
+++ src/build/01B_compile_yx1_images_par.py
@@ -18,6 +18,7 @@ import pickle
 from parfor import pmap
 import pandas as pd
 import time
+import nd2
 
 def scrape_yx1_metadata(im_path):
 
@@ -91,166 +92,96 @@ def trim_image(im, out_shape):
 
 
 
-def process_well(w, imObject, well_list, well_name_list, ff_dir, depth_dir, overwrite_flag=False):
+def process_well(w, im_data_dask, well_name_list, well_time_list, well_ind_list, ff_dir, depth_dir, rs_scale_yx, overwrite_flag=False):
 
     # set scene
-    well_id_string = well_list[w]
-    well_num = int(well_id_string.replace("XYPos:", ""))
-    imObject.set_scene(well_id_string)
-
-    # initialize
-    well_df = pd.DataFrame([], columns=['well', 'nd2_series_num', 'time_int', 'time_string', 'Height (um)', 'Width (um)', 'Height (px)', 'Width (px)', 'Channel', 'Objective', 'Time (s)'])
-
-    # get conventional well name
     well_name_conv = well_name_list[w]
+    time_int = well_time_list[w]
+    well_int = well_ind_list[w]
 
-    # get number of time points
-    n_time_points = 10 # NL: replace with dynamic lookup
-    
-    # each pos dir contains one or more time points
-    for t in range(n_time_points):
+    # get data
+    data_zyx = im_data_dask[time_int, well_int, :, :, :].compute()
 
-        ff_out_name = 'ff_' + well_name_conv + f'_t{t:04}' + '.tif'
+    # generate save names
+    ff_out_name = 'ff_' + well_name_conv + f'_t{time_int:04}' + '.tif'
 
-        if os.path.isfile(ff_out_name) and not overwrite_flag:
-            print(f"Skipping time point {t} for well {well_name_conv}.")
-            continue
-        
-        # get zyx data stack
-        print("Starting to load zyx...")
-        start = time.time()
-        data_zyx_raw = np.squeeze(imObject.get_image_data("CZYX", T=t))
-        print(time.time() - start)
+    if os.path.isfile(ff_out_name) and not overwrite_flag:
+        print(f"Skipping time point {t} for well {well_name_conv}.")
+        continue
 
-        # each time directoy contains a list of Z slices for each channel
-        ch_string = "CH" + str(ch_to_use)
-        im_list = sorted(glob.glob(time_dir + "/*" + ch_string + "*"))
+    # resize image
 
-        # get pixel resolution 
-        pixel_scale_vec = np.asarray(imObject.physical_pixel_sizes)
+    # calculate FF image
+    laps.append(doLap(images[i]))
+    laps_d.append(doLap(images[i], lap_size=7, blur_size=7))  # I've found that depth stacking works better with larger filters
 
-        # check to see if images have already been generated
-        do_flags = [1] * len(sub_pos_index)
-        # print(sub_pos_index)
-        for pi in sub_pos_index:
-            tt = int(time_dir[-4:])
-            
-            if cytometer_flag:
-                # pos_id_list.append(p)
-                pos_string = f'p{p:04}'
-            else:
-                # pos_id_list.append(pi)
-                pos_string = f'p{pi:04}'
+        laps = np.asarray(laps)
+        abs_laps = np.absolute(laps)
 
-            
-            if os.path.isfile(
-                    os.path.join(ff_dir, ff_out_name, 'im_' + pos_string + '.tif')) and not overwrite_flag:
-                do_flags[pi - 1] = 0
-                if t == 0 and p == 0 and w == 0:
-                    print("Skipping pre-existing files. Set 'overwrite_flag=True' to overwrite existing images")
+        laps_d = np.asarray(laps_d)
+        abs_laps_d = np.absolute(laps_d)
 
-        for pi in sub_pos_index:
-            pos_indices = np.where(np.asarray(sub_pos_list) == pi)[0]
-            # load
-            images = []
-            for iter_i, i in enumerate(pos_indices):
-                if do_flags[pi - 1]:
-                    im = cv2.imread(im_list[i])
-                    if im is not None:
-                        images.append(cv2.cvtColor(im, cv2.COLOR_BGR2GRAY))
+        # calculate full-focus and depth images
+        ff_image = np.zeros(shape=images[0].shape, dtype=images[0].dtype)
+        depth_image = np.argmax(abs_laps_d, axis=0)
+        maxima = abs_laps.max(axis=0)
+        bool_mask = abs_laps == maxima
+        mask = bool_mask.astype(np.uint8)
+        for i in range(len(images)):
+            ff_image[np.where(mask[i] == 1)] = images[i][np.where(mask[i] == 1)]
 
-                # scrape metadata from first image
-                if (iter_i == 0) and (p == 0):
-                    temp_dict = scrape_keyence_metadata(im_list[i])
-                    k_list = list(temp_dict.keys())
-                    temp_df = pd.DataFrame(np.empty((1, len(k_list))), columns=k_list)
-                    for k in k_list:
-                        temp_df[k] = temp_dict[k]
-                    
-                    tstring = 'T' + time_dir[-4:]
-                    temp_df["time_string"] = tstring
-                    temp_df["time_int"] = int(time_dir[-4:])
-    
+        # ff_image = 255 - ff_image  # take the negative
 
-                    temp_df["well"] = well_name_conv
-                    temp_df = temp_df[temp_df.columns[::-1]]
-                    # add to main dataframe
-                    well_df.loc[master_iter_i] = temp_df.loc[0]
-                    master_iter_i += 1
+        tt = int(time_dir[-4:])
 
-            if do_flags[pi - 1]:
-                laps = []
-                laps_d = []
-                for i in range(len(images)):
-                    # print
-                    # "Lap {}".format(i)
-                    laps.append(doLap(images[i]))
-                    laps_d.append(doLap(images[i], lap_size=7, blur_size=7))  # I've found that depth stacking works better with larger filters
+        if cytometer_flag:
+            # pos_id_list.append(p)
+            pos_string = f'p{p:04}'
+        else:
+            # pos_id_list.append(pi)
+            pos_string = f'p{pi:04}'
 
-                laps = np.asarray(laps)
-                abs_laps = np.absolute(laps)
+        # save images
+        ff_out_name = 'ff_' + well_name_conv + f'_t{tt:04}_' + f'ch{ch_to_use:02}/'
+        depth_out_name = 'depth_' + well_name_conv + f'_t{tt:04}_' + f'ch{ch_to_use:02}/'
+        op_ff = os.path.join(ff_dir, ff_out_name)
+        op_depth = os.path.join(depth_dir, depth_out_name)
 
-                laps_d = np.asarray(laps_d)
-                abs_laps_d = np.absolute(laps_d)
+        if not os.path.isdir(op_ff):
+            os.makedirs(op_ff)
+        if not os.path.isdir(op_depth):
+            os.makedirs(op_depth)
 
-                # calculate full-focus and depth images
-                ff_image = np.zeros(shape=images[0].shape, dtype=images[0].dtype)
-                depth_image = np.argmax(abs_laps_d, axis=0)
-                maxima = abs_laps.max(axis=0)
-                bool_mask = abs_laps == maxima
-                mask = bool_mask.astype(np.uint8)
-                for i in range(len(images)):
-                    ff_image[np.where(mask[i] == 1)] = images[i][np.where(mask[i] == 1)]
+        # convet depth image to 8 bit
+        max_z = abs_laps.shape[0]
+        depth_image_int8 = np.round(depth_image / max_z * 255).astype('uint8')
 
-                # ff_image = 255 - ff_image  # take the negative
+        cv2.imwrite(os.path.join(ff_dir, ff_out_name, 'im_' + pos_string + '.tif'), ff_image)
 
-                tt = int(time_dir[-4:])
-
-                if cytometer_flag:
-                    # pos_id_list.append(p)
-                    pos_string = f'p{p:04}'
-                else:
-                    # pos_id_list.append(pi)
-                    pos_string = f'p{pi:04}'
-
-                # save images
-                ff_out_name = 'ff_' + well_name_conv + f'_t{tt:04}_' + f'ch{ch_to_use:02}/'
-                depth_out_name = 'depth_' + well_name_conv + f'_t{tt:04}_' + f'ch{ch_to_use:02}/'
-                op_ff = os.path.join(ff_dir, ff_out_name)
-                op_depth = os.path.join(depth_dir, depth_out_name)
-
-                if not os.path.isdir(op_ff):
-                    os.makedirs(op_ff)
-                if not os.path.isdir(op_depth):
-                    os.makedirs(op_depth)
-
-                # convet depth image to 8 bit
-                max_z = abs_laps.shape[0]
-                depth_image_int8 = np.round(depth_image / max_z * 255).astype('uint8')
-
-                cv2.imwrite(os.path.join(ff_dir, ff_out_name, 'im_' + pos_string + '.tif'), ff_image)
-
-                cv2.imwrite(os.path.join(depth_dir, depth_out_name, 'im_' + pos_string + '.tif'), depth_image_int8)
+        cv2.imwrite(os.path.join(depth_dir, depth_out_name, 'im_' + pos_string + '.tif'), depth_image_int8)
 
     # well_dict_out = dict({well_name_conv: well_dict})
 
     return well_df
 
 
-def build_ff_from_yx1(data_root, overwrite_flag=False, ch_to_use=0, dir_list=None, write_dir=None):
+def build_ff_from_yx1(data_root, overwrite_flag=False, ch_to_use=0, dir_list=None, write_dir=None, rs_res=None):
 
     read_dir_root = os.path.join(data_root, 'raw_image_data', 'YX1') 
     if write_dir is None:
         write_dir = os.path.join(data_root, 'built_image_data', 'YX1') 
         
     # handle paths
-    if dir_list == None:
+    if dir_list is None:
         # Get a list of directories
         dir_list_raw = sorted(glob.glob(read_dir_root + "*"))
         dir_list = []
         for dd in dir_list_raw:
             if os.path.isdir(dd):
                 dir_list.append(path_leaf(dd))
+
+    if rs_res is None:
+        rs_res = np.asarray([5.66, 5.66])
 
     # filter for desired directories
     dir_indices = [d for d in range(len(dir_list)) if "ignore" not in dir_list[d]]
@@ -278,10 +209,50 @@ def build_ff_from_yx1(data_root, overwrite_flag=False, ch_to_use=0, dir_list=Non
 
         # Read in experiment metadata 
         print(f"Processing {sub_name}...")
-        imObject = AICSImage(image_list[0])       
-        n_wells = len(imObject.scenes)
-        well_list = imObject.scenes
+        # imObject = AICSImage(image_list[0])       
+        # n_wells = len(imObject.scenes)
+        # well_list = imObject.scenes
         # n_time_points = imObject.dims["T"][0]
+
+        imObject= nd2.ND2File(image_list[0])
+        im_shape = imObject.shape
+        n_time_points = im_shape[0]
+        n_wells = im_shape[1]
+        n_z_slices = im_shape[2]
+
+        # pull dask array
+        im_array_dask = imObject.to_dask()
+        # use first 10 frames to infer time resolution
+
+        # extract frame times
+        n_frames_total = imObject.frame_metadata(0).contents.frameCount
+        frame_time_vec = [imObject.frame_metadata(i).channels[0].time.relativeTimeMs / 1000 for i in
+                        range(0, n_frames_total, im_shape[2])]
+        # check for common nd2 artifact where time stamps jump midway through
+        dt_frame_approx = (imObject.frame_metadata(n_z_slices).channels[0].time.relativeTimeMs -
+                        imObject.frame_metadata(0).channels[0].time.relativeTimeMs) / 1000
+        jump_ind = np.where(np.diff(frame_time_vec) > 2*dt_frame_approx)[0] # typically it is multiple orders of magnitude to large
+        if len(jump_ind) > 0:
+            jump_ind = jump_ind[0]
+            # prior to this point we will just use the time stamps. We will extrapolate to get subsequent time points
+            nf = jump_ind - 1 - int(jump_ind/2)
+            dt_frame_est = (frame_time_vec[jump_ind-1] - frame_time_vec[int(jump_ind/2)]) / nf
+            base_time = frame_time_vec[jump_ind-1]
+            for f in range(jump_ind, len(frame_time_vec)):
+                frame_time_vec[f] = base_time + dt_frame_est*(f - jump_ind)
+        frame_time_vec = np.asarray(frame_time_vec)
+
+        # get image resolution
+        voxel_size = imObject.voxel_size()
+        voxel_yx = np.asarray([voxel_size[1], voxel_size[0]])
+        rs_factor = np.divide(voxel_yx, rs_res)
+
+        rs_dims_yx = np.round(np.multiply(np.asarray(im_shape[3:]), rs_factor)).astype(int)
+        # resample images to a standardized resolution
+
+
+        # # initialize metadata data frame
+        # well_df = pd.DataFrame([], columns=['well', 'nd2_series_num', 'microscope', 'time_int', 'Height (um)', 'Width (um)', 'Height (px)', 'Width (px)', 'Objective', 'Time (s)'])
 
         # read in plate map
         plate_map_xl = pd.ExcelFile(dir_path + "plate_map.xlsx")
@@ -301,12 +272,34 @@ def build_ff_from_yx1(data_root, overwrite_flag=False, ch_to_use=0, dir_list=Non
 
         si = np.argsort(well_ind_list)
         well_name_list_sorted = np.asarray(well_name_list)[si].tolist()
+
+        # generate longform vectors
+        well_name_list_long = np.repeat(well_name_list_sorted, n_time_points)
+        well_ind_list_long = np.repeat(well_ind_list, n_time_points)
+
+        # generate metadata dataframe
+        well_df = pd.DataFrame(well_name_list_long[:, np.newaxis], columns=["well"])
+        well_df["nd2_series_num"] = well_ind_list_long
+        well_df["microscope"] = "YX1"
+        time_int_list = np.tile(np.arange(1, n_time_points+1), n_wells)
+        well_int_list = np.repeat(np.arange(1, n_wells+1), n_time_points)
+        well_df["time_int"] = time_ind_list
+        well_df["Height (um)"] = im_shape[3]*voxel_size[1]
+        well_df["Width (um)"] = im_shape[4]*voxel_size[0]
+        well_df["Height (px)"] = rs_dims_yx[0]
+        well_df["Width (px)"] = rs_dims_yx[1]
+        well_df["Channel"] = imObject.frame_metadata(0).channels[0].channel.name
+        well_df["Objective"] = imObject.frame_metadata(0).channels[0].microscope.objectiveName
+        time_ind_vec = []
+        for n in range(n_wells):
+            time_ind_vec += np.arange(n, n_wells*n_time_points, n_wells).tolist()
+        well_df["Time (s)"] = frame_time_vec[time_ind_vec]
+
         print(f'Building full-focus images in directory {d+1:01} of ' + f'{len(dir_indices)}')
         # metadata_df_list = pmap(process_well, range(len(well_list)), (well_list, cytometer_flag, ff_dir, depth_dir, ch_to_use, overwrite_flag), rP=0.5)
-        metadata_df_list = []
-        for w in range(n_wells):
-            metadata_well = process_well(w, imObject, well_list, well_name_list_sorted, ff_dir, depth_dir, ch_to_use=1, overwrite_flag=False)
-            metadata_df_list.append(metadata_well)
+        for i in range(n_wells*n_time_points):
+            process_well(w, im_array_dask, well_name_list_long, time_int_list, well_int_list, ff_dir, depth_dir, ch_to_use=1, overwrite_flag=False)
+    
 
 
         if len(metadata_df_list) > 0:
