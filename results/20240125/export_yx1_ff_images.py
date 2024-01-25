831f9df24fa64a52147621942dfbcecb9aa10266
nlammers371 <nlammers@sage001.grid.gs.washington.edu>
2024-01-25 14:31:36 -0800

    Fixed a couple of bugs in the YX1 build

---
+++ results/20240125/export_yx1_ff_images.py
@@ -0,0 +1,8 @@
+from src.build.build01B_compile_yx1_images_torch import build_ff_from_yx1
+
+overwrite_flag = True
+data_root = "/net/trapnell/vol1/home/nlammers/projects/data/morphseq/"
+dir_list = ["20231110", "20231218", "20231206"]
+
+# build FF images
+build_ff_from_yx1(data_root=data_root, dir_list=dir_list, overwrite_flag=overwrite_flag, metadata_only_flag=False)
