86e85b4f9f238a9ae76873d02d46131a3e01c562
nlammers371 <nlammers@grid-head1.grid.gs.washington.edu>
2024-02-03 09:40:08 -0800

    Need to update that gitignore file...

--- src/vae/auxiliary_scripts/assess_image_set.py
+++ src/vae/auxiliary_scripts/assess_image_set.py
@@ -4,13 +4,13 @@ import os
 import skimage.io as io
 from skimage.transform import rescale
 from src.vae.models.auto_model import AutoModel
-from src.functions.dataset_utils import set_inputs_to_device
 import matplotlib.pyplot as plt
 import umap.umap_ as umap
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 import torch.nn.functional as F
 import pandas as pd
+from pythae.data.datasets import collate_dataset_output
 from tqdm import tqdm
 # from pythae.trainers.base_trainer_verbose import base_trainer_verbose
 #from src.functions.dataset_utils import ContrastiveLearningDataset, ContrastiveLearningViewGenerator
@@ -19,6 +19,24 @@ from torch.utils.data import DataLoader
 import json
 from typing import Any, Dict, List, Optional, Union
 import ntpath
+from src.vae.auxiliary_scripts.assess_vae_results import calculate_UMAPs
+
+
+def set_inputs_to_device(device, inputs: Dict[str, Any]):
+    inputs_on_device = inputs
+
+    if device == "cuda":
+        cuda_inputs = dict.fromkeys(inputs)
+
+        for key in inputs.keys():
+            if torch.is_tensor(inputs[key]):
+                cuda_inputs[key] = inputs[key].cuda()
+
+            else:
+                cuda_inputs[key] = inputs[key]
+        inputs_on_device = cuda_inputs
+
+    return inputs_on_device
 
 # Script to generate image reconstructions and latent space projections for a designated set of embryo images
 def assess_image_set(image_path, metadata_path, trained_model_path, out_path, image_prefix_list="", rs_factor=1.0, batch_size=64):
@@ -58,22 +76,39 @@ def assess_image_set(image_path, metadata_path, trained_model_path, out_path, im
     keep_indices = np.asarray([i for i in range(len(snip_id_vec)) if snip_id_vec[i] in image_snip_names])
 
     embryo_df = embryo_df.loc[keep_indices, :].copy()
+    embryo_df.reset_index(inplace=True)
 
     # load and store the images
     print("Loading images...")
+    input_image_path = os.path.join(out_path, "input_images", "class0")
+    if not os.path.isdir(input_image_path):
+        os.makedirs(input_image_path)
+
     for i in tqdm(range(len(image_path_list))):
         img_raw = io.imread(image_path_list[i])
+        if len(img_raw.shape) == 3:
+            img_raw = img_raw[:, :, 0]
+        
         if rs_flag:
-            img = torch.from_numpy(rescale(img_raw.astype(np.float16), rs_factor, anti_aliasing=True))
+            img = rescale(img_raw.astype(np.float16), rs_factor, preserve_range=True, anti_aliasing=True).astype(np.uint8)
         else:
-            img = torch.from_numpy(img_raw)
+            img = img_raw
+        im_name = path_leaf(image_path_list[i])
+        # save
+        io.imsave(fname=os.path.join(input_image_path, im_name), arr=img)
 
-        if i == 0:
-            # generate tensor to hold images (I'm assuming they will be few enough to hold in memory)
-            rs_dims = img.shape
-            image_tensor = torch.zeros((len(image_snip_names), 1, rs_dims[0], rs_dims[1]))
-
-        image_tensor[i, 0, :, :] = img
+    data_transform = make_dynamic_rs_transform()
+    dataset = MyCustomDataset(
+            root=os.path.join(out_path, "input_images"),
+            transform=data_transform,
+            return_name=True
+        )
+    dataloader = DataLoader(
+                    dataset=dataset,
+                    batch_size=batch_size,
+                    shuffle=False,
+                    collate_fn=collate_dataset_output,
+                )
     # initialize latent variable columns
     new_cols = []
     for n in range(trained_model.latent_dim):
@@ -100,15 +135,13 @@ def assess_image_set(image_path, metadata_path, trained_model_path, out_path, im
         os.makedirs(recon_fig_path)
 
     print("Extracting latent space vectors and testing image reconstructions...")
-    trained_model = set_inputs_to_device(trained_model, device)
+    trained_model = trained_model.to(device)
     # get the dataloader
-    n_images = image_tensor.shape[0]
-    n_batches = int(np.ceil(n_images/batch_size))
     start_i = 0
-    for n in tqdm(range(n_batches)):
-        stop_i = np.min([n_images, start_i+batch_size])
-        inputs = image_tensor[start_i:stop_i]
-        x = set_inputs_to_device(inputs, device)
+    for n, inputs in enumerate(tqdm(dataloader)):
+
+        inputs = set_inputs_to_device(device, inputs)
+        x = inputs["data"]
 
         encoder_output = trained_model.encoder(x)
         mu, log_var = encoder_output.embedding, encoder_output.log_covariance
@@ -128,10 +161,12 @@ def assess_image_set(image_path, metadata_path, trained_model_path, out_path, im
         # encoder_output = encoder_output.detach().cpu()
         # ###
         # # Add recon loss and latent encodings to the dataframe
+        stop_i = np.min([start_i + batch_size, len(dataset)])
         iter_snips = image_snip_names[start_i:stop_i]
         df_ind_vec = np.asarray([np.where(embryo_df.loc[:, "snip_id"] == snip)[0][0] for snip in iter_snips])
         embryo_df.loc[df_ind_vec, "recon_mse"] = np.asarray(recon_loss)
 
+        start_i += batch_size
         # add latent encodings
         zm_array = np.asarray(encoder_output[0].detach().cpu())
 
@@ -144,32 +179,42 @@ def assess_image_set(image_path, metadata_path, trained_model_path, out_path, im
             else:
                 embryo_df.loc[df_ind_vec, f"z_mu_{z:02}"] = zm_array[:, z]
 
-        # for b in range(len(y)):
-        #
-        #     if not skip_figures:
-        #         if fig_counter < n_image_figs:
-        #             # show results with normal sampler
-        #             fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(10, 10))
-        #
-        #             axes[0].imshow(np.squeeze(np.squeeze(x[b, 0, :, :])), cmap='gray')
-        #             axes[0].axis('off')
-        #
-        #             axes[1].imshow(np.squeeze(np.squeeze(recon_x_out[b, 0, :, :])), cmap='gray')
-        #             axes[1].axis('off')
-        #
-        #             plt.tight_layout(pad=0.)
-        #
-        #             plt.savefig(
-        #                 os.path.join(image_path, y[b] + f'_loss{int(np.round(recon_loss[b], 0)):05}.jpg'))
-        #             plt.close()
-        #
-        #             fig_counter += 1
+        for b in range(x.shape[0]):
 
+            # show results with normal sampler
+            fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(10, 10))
+
+            axes[0].imshow(np.squeeze(np.squeeze(x.detach().cpu()[b, 0, :, :])), cmap='gray')
+            axes[0].axis('off')
+
+            axes[1].imshow(np.squeeze(recon_x_out[b, 0, :, :]), cmap='gray')
+            axes[1].axis('off')
+
+            plt.tight_layout(pad=0.)
+
+            plt.savefig(
+                os.path.join(recon_fig_path, image_snip_names[b] + '_loss.jpg'))
+            plt.close()
+
+            # save just the recon on its own
+            int_recon_out = (np.squeeze(np.asarray(recon_x_out[b, 0, :, :]))*255).astype(np.uint8)
+            io.imsave(fname=os.path.join(image_path, image_snip_names[b] + '_loss.jpg'), arr=int_recon_out)
+
+
+    # now fit 2 and 3-dimensional UMAP models 
+    print("Calculating UMAPS...")
+    embryo_df = calculate_UMAPs(embryo_df)
+
+    print("Saving...")
+    embryo_df.to_csv(os.path.join(out_path, "embryo_df.csv"))
+       
 
 if __name__ == "__main__":
 
-    root = "E:\\Nick\\Cole Trapnell's Lab Dropbox\\Nick Lammers\\Nick\\morphseq"
+    # root = "E:\\Nick\\Cole Trapnell's Lab Dropbox\\Nick Lammers\\Nick\\morphseq"
+    root = "/net/trapnell/vol1/home/nlammers/projects/data/morphseq"
     image_path = os.path.join(root, "training_data", "bf_embryo_snips")
+    # image_path = os.path.join(root, "training_data", "20231106_ds", "train", "20230525")
     metadata_path = os.path.join(root, "metadata")
     out_path = os.path.join(root, "analysis", "lmx1b")
     prefix_list = ["20230830", "20230831", "20231207", "20231208"]
