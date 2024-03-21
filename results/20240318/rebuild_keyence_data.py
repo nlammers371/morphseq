441867ab20696bd643e839917e8716551338e14a
nlammers371 <nlammers@t006.grid.gs.washington.edu>
2024-03-21 13:34:51 -0700

    fixing bugs in keyence stitch script

--- results/20240318/rebuild_keyence_data.py
+++ results/20240318/rebuild_keyence_data.py
@@ -1,13 +1,13 @@
 from src.build.build01A_compile_keyence_images import build_ff_from_keyence, stitch_ff_from_keyence
 
 
-overwrite_flag = False
+overwrite_flag = True
 
 data_root = "/net/trapnell/vol1/home/nlammers/projects/data/morphseq/"
 dir_list = None
 n_workers = 8
 # build FF images
-build_ff_from_keyence(data_root, n_workers=n_workers, par_flag=True, overwrite_flag=overwrite_flag, dir_list=dir_list)
+# build_ff_from_keyence(data_root, n_workers=n_workers, par_flag=True, overwrite_flag=overwrite_flag, dir_list=dir_list)
 
 # stitch FF images
 stitch_ff_from_keyence(data_root, n_workers=n_workers, par_flag=n_workers>1, overwrite_flag=overwrite_flag, dir_list=dir_list)
