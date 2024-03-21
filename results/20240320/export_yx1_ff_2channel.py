b4e923f9b3e861370acfd5edf779e1c4fc7ef64f
nlammers371 <nlammers@berkeley.edu>
2024-03-21 06:59:23 -0700

    Updates to YX1 build to accomodate fluorescent markers

---
+++ results/20240320/export_yx1_ff_2channel.py
@@ -0,0 +1,9 @@
+from src.build.build01B_compile_yx1_images_torch import build_ff_from_yx1
+
+overwrite_flag = True
+# data_root = "/net/trapnell/vol1/home/nlammers/projects/data/morphseq/"
+data_root = "E:/Nick/Cole Trapnell's Lab Dropbox/Nick Lammers/Nick/morphseq/"
+dir_list = ["20240314"]
+# reducing kept z slices because of OOF issue with 1206 experiment
+# build FF images
+build_ff_from_yx1(data_root=data_root, dir_list=dir_list, overwrite_flag=overwrite_flag, metadata_only_flag=False, n_z_keep_in=None)
