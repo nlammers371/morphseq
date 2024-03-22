3fe994563aa1ea5279b2bbff83f543e61f9a401d
nlammers371 <nlammers@t006.grid.gs.washington.edu>
2024-03-21 17:41:10 -0700

    Finished patching up keyence export

--- results/20240318/rebuild_keyence_data.py
+++ results/20240318/rebuild_keyence_data.py
@@ -5,13 +5,13 @@ import multiprocessing
 def main():
     overwrite_flag = True
     data_root = "/net/trapnell/vol1/home/nlammers/projects/data/morphseq/"
-    dir_list = ["20230830", "20230831", "20231207", "20231208"]
+    dir_list = ["20231208"]
     n_workers = 4
     # build FF images
-    build_ff_from_keyence(data_root, n_workers=n_workers, par_flag=True, overwrite_flag=overwrite_flag, dir_list=dir_list)
+    build_ff_from_keyence(data_root, n_workers=n_workers, par_flag=False, overwrite_flag=overwrite_flag, dir_list=dir_list)
 
     # stitch FF images
-    stitch_ff_from_keyence(data_root, n_workers=n_workers, par_flag=True, overwrite_flag=overwrite_flag, dir_list=dir_list)
+    stitch_ff_from_keyence(data_root, n_workers=n_workers, par_flag=False, overwrite_flag=overwrite_flag, dir_list=dir_list)
 
 if __name__ == '__main__':
     multiprocessing.freeze_support()
