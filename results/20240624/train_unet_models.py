02b148454c09c927d4aa45203bd90b8dceaebd69
Nick <nlammers@berkeley.edu>
2024-06-25 11:33:16 -0700

    Minor changes

--- results/20240624/train_unet_models.py
+++ results/20240624/train_unet_models.py
@@ -5,12 +5,12 @@ from src.ml_preprocessing.train_embryo_morph_segmenter import train_unet_classif
 def main():
 
 
-    model_type_vec = ["mask", "via", "yolk", "bubble", "focus"]
-    model_name_vec = ["mask_v0_", "via_v0_", "yolk_v0_", "bubble_v0_", "focus_v0_"] #, "bubble_v0", "focus_v0"]
+    model_type_vec = ["via", "yolk", ]
+    model_name_vec = ["via_v1_", "yolk_v1_", ] #, "bubble_v0", "focus_v0"]
 
     # option to load previously trained model
     # pretrained_model = "/media/nick/hdd02/Cole Trapnell's Lab Dropbox/Nick Lammers/Nick/morphseq/built_image_data/segmentation_models/20240507/unet_yolk_v0_0050"
-    n_epochs = 100
+    n_epoch_vec = [100, 50]
     # Set path to data
     root = "/media/nick/hdd02/Cole Trapnell's Lab Dropbox/Nick Lammers/Nick/morphseq/built_image_data/"
 
@@ -18,7 +18,7 @@ def main():
         model_name = model_name_vec[m]
         model_type = model_type_vec[m]
         train_unet_classifier(image_root=root, model_type=model_type, model_name=model_name,
-                              n_epoch=n_epochs, pretrained_model=None)
+                              n_epoch=n_epoch_vec[m], pretrained_model=None)
 
 if __name__ == '__main__':
     main()
