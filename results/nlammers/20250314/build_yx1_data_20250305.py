c64db40d6cce4d5eb0eecef6e507f64f979dd629
nlammers371 <nlammers@t002.grid.gs.washington.edu>
2025-03-14 13:22:11 -0700

    Remove ignored files

---
+++ results/nlammers/20250314/build_yx1_data_20250305.py
@@ -0,0 +1,24 @@
+import os
+import sys
+
+code_root = "/net/trapnell/vol1/home/nlammers/projects/repositories/morphseq"
+sys.path.insert(0, code_root)
+
+from src.build.build01B_compile_yx1_images_torch import build_ff_from_yx1
+import multiprocessing
+
+def main():
+    # overwrite_flag = False
+    data_root = "/net/trapnell/vol1/home/nlammers/projects/data/morphseq/"
+    # data_root = "E:/Nick/Cole Trapnell's Lab Dropbox/Nick Lammers/Nick/morphseq/"
+    dir_list = ["20250305"]
+    n_z_keep_vec = [None]
+    # reducing kept z slices because of OOF issue with 1206 experiment
+    # build FF images
+    build_ff_from_yx1(data_root=data_root, overwrite_flag=False, n_z_keep_in=n_z_keep_vec, metadata_only_flag=False, dir_list=dir_list,
+                      par_flag=False)
+
+
+if __name__ == '__main__':
+    multiprocessing.freeze_support()
+    main()
