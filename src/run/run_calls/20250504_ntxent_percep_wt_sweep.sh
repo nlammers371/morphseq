b2cc8c8ea800c6bff6b6c6f470bffd17f646f273
Nick <nlammers@berkeley.edu>
2025-05-04 23:42:52 -0700

    new bash calls for ntxent testing

--- src/run/run_calls/20250504_ntxent_percep_wt_sweep.sh
+++ src/run/run_calls/20250504_ntxent_percep_wt_sweep.sh
@@ -11,15 +11,13 @@ python -m src.run.training_cluster --run \
 # === Second run ===
 echo ">>> Starting second run: run_name=${2:-runB}"
 python -m src.run.training_cluster --run \
-    hydra.job.name=ntxent_00_n50_T01_bio \
-    model=morph_vae_big_cluster \
-    model.lossconfig.metric_weight=50.0 \
-    model.lossconfig.pips_weight=0.8 \
-    model.ddconfig.n_out_channels=16 \
-    model.ddconfig.frac_nuisance_latents=0.5 \
-    model.dataconfig.batch_size=256 \
-    model.lossconfig.kld_weight=5.0 \
-    model.lossconfig.bio_only_kld=True \
-    model.trainconfig.max_epochs=125
+    hydra.job.name=ntxent_percep005 \
+    model.lossconfig.pips_weight=0.05
+
+
+echo ">>> Starting second run: run_name=${3:-runB}"
+python -m src.run.training_cluster --run \
+    hydra.job.name=ntxent_percep001 \
+    model.lossconfig.pips_weight=0.01
 
 echo "✅ Both runs complete."
