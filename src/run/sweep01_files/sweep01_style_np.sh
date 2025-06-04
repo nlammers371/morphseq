2f627d7e7020c0d3248d005ef19d1908674beffc
Nick <nlammers@berkeley.edu>
2025-06-03 18:52:15 -0700

    Weird

--- src/run/sweep01_files/sweep01_style_np.sh
+++ src/run/sweep01_files/sweep01_style_np.sh
@@ -6,6 +6,6 @@ echo ">>> Starting first run: run_name=${1:-runA}"
 python -m src.run.training --multirun \
     hydra.job.name=sweep01_style_np \
     model=vae_timm_no_pips \
-    model.ddconfig.name="Efficient-B0-RA","Efficient-B4","ConvNeXt-Tiny","Swin-Tiny","MaxViT-Tiny" \
+    model.ddconfig.name="Efficient-B0-RA","Efficient-B4","ConvNeXt-Tiny","Swin-Tiny","MaxViT-Tiny","Swin-Large","MaxViT-Small","Vit-Large" \
     model.lossconfig.gan_net="style2" \
     model.lossconfig.gan_weight=0.05,0.25
