00c84bac8cde041e954e55b27aee1ddef84b380c
nlammers371 <nlammers@t009.grid.gs.washington.edu>
2025-06-06 15:09:31 -0700

    Wrecked a lot of lightning logs

--- results/nlammers/sweep02/make_recon_examples.py
+++ results/nlammers/sweep02/make_recon_examples.py
@@ -9,10 +9,13 @@ if __name__ == "__main__":
     # root = "/media/nick/hdd021/Cole Trapnell's Lab Dropbox/Nick Lammers/Nick/morphseq/training_data/20241107_ds/"
     root = "/net/trapnell/vol1/home/nlammers/projects/data/morphseq/training_data/20241107_ds/"
     tr_root = os.path.join(root, "training_outputs")
-    dir_list = sorted(glob(tr_root + "sweep02_*"))
+    dir_list = sorted(glob(tr_root + "/sweep02_*"))
     out_path = os.path.join(root, "sweep02_figs")
     for dir_name in dir_list:
-        hydra_path = os.path.join(tr_root, dir_name, "")
-        recon_wrapper(hydra_run_path=hydra_path,
-                      out_path=out_path,
-                      run_type="multirun")
+        try:
+            hydra_path = os.path.join(tr_root, dir_name, "")
+            recon_wrapper(hydra_run_path=hydra_path,
+                        out_path=out_path,
+                        run_type="multirun")
+        except:
+            print(f"Skipping {dir_name}")
