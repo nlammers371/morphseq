5339443582a1bb8e174baca753bae8aacb2fe345
nlammers371 <nlammers@t008.grid.gs.washington.edu>
2025-06-08 10:04:23 -0700

    Son of a bitch

--- src/run/sweep03_files/sweep03_ViTT.sh
+++ src/run/sweep03_files/sweep03_ViTT.sh
@@ -2,10 +2,10 @@
 set -euo pipefail
 
 # === First run ===
-echo ">>> Starting first run: run_name=${1:-ViTT}"
+echo ">>> Starting first run: run_name=${1:-ViTL}"
 python -m src.run.training_cluster --multirun \
-    hydra.job.name=sweep03_ViTT \
+    hydra.job.name=sweep03_ViTL \
     model=vae_timm \
-    model.ddconfig.name="ViT-Tiny"  \
+    model.ddconfig.name="ViT-Large"  \
     model.lossconfig.gan_net="ms_patch","style2_big" \
     model.lossconfig.gan_weight=0.05,0.01
