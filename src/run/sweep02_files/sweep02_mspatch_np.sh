a56093303ced9d73de0d6e243451c8191e048ad8
Nicholas Lammers <nlammers@berkeley.edu>
2025-06-09 13:16:02 -0700

    Folder rename

--- src/run/sweep02_files/sweep02_mspatch_np.sh
+++ src/run/sweep02_files/sweep02_mspatch_np.sh
@@ -6,6 +6,10 @@ echo ">>> Starting first run: run_name=${1:-runA}"
 python -m src.run.training_cluster --multirun \
     hydra.job.name=sweep02_mspatch_np \
     model=vae_timm_no_pips \
-    model.ddconfig.name="Efficient-B0-RA","Efficient-B4","ConvNeXt-Tiny","Swin-Tiny","Swin-Large","RegNet-Y","Vit-Large" \
+    model.ddconfig.name="Efficient-B0-RA","Efficient-B4","ConvNeXt-Tiny","Swin-Tiny","Swin-Large","RegNet-Y","ViT-Large" \
     model.lossconfig.gan_net="ms_patch" \
     model.lossconfig.gan_weight=0.05,0.25
+
+conda activate torch-env
+cd projects/repositories/morphseq
+./src/run/sweep03_files/sweep03
