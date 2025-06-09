717a5de24dc9ff14dac8c9f9d3aa1b2402d75141
nlammers371 <nlammers@nexus3.gs.washington.edu>
2025-06-09 13:11:35 -0700

    script to check results for sweep03

---
+++ results/nlammers/20250609_sweep03/make_recon_examples.py
@@ -0,0 +1,16 @@
+from src.assess.get_recon_examples import recon_wrapper
+import os
+from glob2 import glob
+
+if __name__ == "__main__":
+
+    root = "/media/nick/hdd021/Cole Trapnell's Lab Dropbox/Nick Lammers/Nick/morphseq/training_data/20241107_ds/"
+    # root = "/net/trapnell/vol1/home/nlammers/projects/data/morphseq/training_data/20241107_ds/"
+    tr_root = os.path.join(root, "training_outputs")
+    dir_list = sorted(glob(os.path.join(tr_root, "sweep03_*")))
+    out_path = os.path.join(root, "sweep03_figs")
+    for dir_name in dir_list:
+        hydra_path = os.path.join(tr_root, dir_name, "")
+        recon_wrapper(hydra_run_path=hydra_path,
+                      out_path=out_path,
+                      run_type="multirun")
